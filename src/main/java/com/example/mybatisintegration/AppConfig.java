8bf77fc5921d8189b0602d75c6a2a246f85d8b3f
qq0012520 <qq0012520@163.com>
2019-02-18 21:33:54 +0800

    commit test

--- src/main/java/com/example/mybatisintegration/AppConfig.java
+++ src/main/java/com/example/mybatisintegration/AppConfig.java
@@ -1,10 +1,5 @@
 package com.example.mybatisintegration;
 
-import com.p6spy.engine.spy.P6DataSource;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
@@ -12,9 +7,4 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
 
 
- /*   @Bean
-    public P6DataSource setDataSource(DataSource dataSource){
-        return new P6DataSource(dataSource);
-    }
-*/
 }
